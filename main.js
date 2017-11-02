b32561b2fbb9c93504289ec6a4aa9342a40514fa
Douglas Kincaid <DgKincaid@users.noreply.github.com>
2017-11-01 21:52:17 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,48 @@
+var express = require('express');
+var cheerio = require('cheerio');
+var fs = require('fs');
+var request = require('request');
+var app = express();
+
+let url = process.argv[2];
+
+console.log(url);
+
+function aquire(){
+    request(url, function(error, response, html){
+        if(!error){
+            var $ = cheerio.load(html);
+            var json = { title: '' };
+
+            $('.b-story-header').filter(function(){
+                var data = $(this);
+
+                json.title = data.children().first().text();
+            })
+
+            $('.b-story-body-x').filter(function(){
+                console.log('body');
+                var data = $(this);
+
+                saveToFile(data.children().first().text())
+            })
+        }
+        else{
+            console.log('Error: ' + error);
+        }
+    })
+}
+
+function saveToFile(text){
+    fs.writeFile("/tmp/test.txt", text, function(err) {
+        if(err) {
+            return console.log(err);
+        }
+    
+        console.log("The file was saved!");
+    }); 
+}
+
+if(url){
+    aquire();
+}
